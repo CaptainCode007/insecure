3b0ade355bfbe84b8f56f7c3d803aa95ffc2e9da
Mrunal Shah <mrunalshah6@gmail.com>
2024-04-27 21:58:08 -0400

    removed provider and moved it to main for sanity

--- main.tf
+++ main.tf
@@ -1,71 +1,22 @@
-resource "aws_vpc" "main" {
-  cidr_block = var.cidr_block
+
+terraform {
+    required_providers {
+        aws = {
+            source  = "hashicorp/aws"
+            version = ">= 3.0.0"
+        }
+    }
+
+    backend "s3" {
+        bucket         = "terraform-state-mrunal"
+        key            = "terraform.tfstate"
+        region         = "us-east-1"
+       
+    }
 }
 
-resource "aws_subnet" "public" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = var.subnet_cidr_block
-}
-
-resource "aws_internet_gateway" "gw" {
-  vpc_id = aws_vpc.main.id
-}
-
-resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.main.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.gw.id
-  }
-}
-
-resource "aws_subnet" "private1" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = var.private1_cidr_block
-  availability_zone = var.availability_zone_az1
-  tags = {
-    "kubernetes.io/cluster/wiz-demo" = 1
-    
-  }
-}
-
-resource "aws_subnet" "private2" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = var.private2_cidr_block
-  availability_zone = var.availability_zone_az2
-  tags = {
-    "kubernetes.io/cluster/wiz-demo" = 1
-    
-  }
-}
-resource "aws_route_table_association" "public" {
-  subnet_id      = aws_subnet.public.id
-  route_table_id = aws_route_table.public.id
-}
-
-resource "aws_eip" "nat" {
-  vpc = true
-}
-
-resource "aws_nat_gateway" "nat" {
-  allocation_id = aws_eip.nat.id
-  subnet_id     = aws_subnet.public.id
-  depends_on    = [aws_internet_gateway.gw]
-}
-
-resource "aws_route_table" "private" {
-  vpc_id = aws_vpc.main.id
-
-  route {
-    cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.nat.id
-  }
-}
-
-resource "aws_route_table_association" "private" {
-  subnet_id      = aws_subnet.private1.id
-  route_table_id = aws_route_table.private.id
+provider "aws" {
+    region = var.region
 }
 
 
--- s3.tf
+++ s3.tf
@@ -1,5 +1,14 @@
 resource "aws_s3_bucket" "bucket" {
-  bucket = "mrunal-public-bucket" # replace with your bucket name
+  bucket = "my-bucket-mrunal" # replace with your bucket name
+}
+
+resource "aws_s3_bucket_public_access_block" "access_block" {
+  bucket = aws_s3_bucket.bucket.id
+
+  block_public_acls   = false
+  block_public_policy = false
+  ignore_public_acls  = false
+  restrict_public_buckets = false
 }
 
 resource "aws_s3_bucket_policy" "bucket_policy" {
